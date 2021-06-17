3b68bcd299c1b2601fe524b3d1faf9f2106cd6b2
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-06-17 17:31:48 -0300

    feat: added local storage functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import Form from "../avances/Form";
 import ToDoList from "./components/ToDoList";
 
 const App = () => {
@@ -8,7 +8,20 @@ const App = () => {
   const [inputText, setInputText] = useState("");
   const [status, setStatus] = useState("all");
   const [filteredTasks, setFilteredTasks] = useState([]);
-     
+ 
+   useEffect(() => {
+     const getLocalTasks = (()=>{
+       if(localStorage.getItem('tasks') == null){
+         localStorage.setItem("tasks", JSON.stringify(tasks))
+       } else{
+         const tasksLocal = JSON.parse(localStorage.getItem('tasks'));
+         setTasks(tasksLocal);
+       } 
+     }
+     ) 
+    getLocalTasks();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
   useEffect(() => {
     const handlerFilter = () => {
@@ -24,9 +37,14 @@ const App = () => {
           break;
       }
     };
-
+    const saveLocalTasks = () => {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+    saveLocalTasks();
     handlerFilter();
   }, [tasks, status]);
+
+
   return (
     <>
       <header>
