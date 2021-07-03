602d3a0695c510391d632dc67f704822e37a91b6
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-07-03 11:40:53 -0300

    feat: styles improved with material ui

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./firebase-config";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary:{
+      main: '#6d6875'
+    },
+    secondary: {
+      main: "#ffb4a2",
+    },
+    textPrimary: {
+      main: "#ffcdb2",
+    },
+    secondaryBackground: {
+      main: "#b5838d",
+    },
+    danger: {
+      main: "#e5989b",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
